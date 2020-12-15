a3e81afd16133db07e2102afb0bb31ac853a64ed
Ramon Reyes <37812464+ramon-rr@users.noreply.github.com>
2020-12-15 13:17:37 -0700

    Initial comment

--- app/src/main/java/com/example/kettle/MainActivity.java
+++ app/src/main/java/com/example/kettle/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Kettle Main Class Entry point to Frags
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
